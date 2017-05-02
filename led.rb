7e5d36167e0f67080fafed1cc243e96e05c9dbfd
Sander Jansen <sanderjansen1997@hotmail.com>
2017-05-02 10:04:40 +0200

    RGB is now dynamic (could still have some bugs)

--- led.rb
+++ led.rb
@@ -2,15 +2,16 @@ class Led
     attr_accessor :color
 
     def calculate_color (temperature, ideal_temperature, range)
-        if temperature <= (ideal_temperature - range)
-            #@color = "0000FF"
-            @color = 0x0000FF
-        elsif temperature >= (ideal_temperature + range)
-            #@color = "FF0000"
-            @color = 0xFF0000
+        colorstep = 255 / ((ideal_temperature + range) - (ideal_temperature - range))
+        gradient = temperature - (ideal_temperature-range)
+        hexcolor = colorstep * gradient
+
+        if temperature < (ideal_temperature - range)
+            @color = "0000#{hexcolor.abs.to_s(16)}"
+        elsif temperature > (ideal_temperature + range)
+            @color = "#{hexcolor.abs.to_s(16)}0000"
         else
-            #@color = "00FF00"
-            @color = 0x00FF00
+            @color = "00#{hexcolor.abs.to_s(16)}00"
         end
     end
 
@@ -18,3 +19,11 @@ class Led
         return @color
     end
 end
+
+
+# Calculate dynamic color
+# ideal +- range
+# ex. 20 range 2 --> 18 and 22 within range
+# color step = 255 / range steps(ideal max - ideal min)
+# gradient = current temp - (min ideal)
+# hex color = colorstep * gradient
